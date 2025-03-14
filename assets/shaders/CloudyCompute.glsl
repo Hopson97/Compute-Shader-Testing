0c75fbbdfcda866c719c4b04bd13a6b96ef5deac
Matthew Hopson <mhopson@hotmail.co.uk>
2025-03-14 22:12:43 +0000

    Add more cloudy

--- assets/shaders/CloudyCompute.glsl
+++ assets/shaders/CloudyCompute.glsl
@@ -6,10 +6,22 @@ layout(rgba32f, binding = 0) uniform writeonly image2D out_image;
 uniform mat4 inv_projection;
 uniform mat4 inv_view;
 uniform vec3 position;
+uniform float time;
 
 #define MAX_RAY_LENGTH 100.0
-#define MARCH_SIZE 0.08
+#define MARCH_SIZE 0.1
 #define MAX_STEPS 80
+
+mat2 rotate_2d(float angle)
+{
+    float c = cos(angle);
+    float s = sin(angle);
+    return 
+        mat2(
+            c, -s,
+            s, c
+        );
+}
 
 struct Ray 
 {
@@ -17,6 +29,77 @@ struct Ray
 	vec3 direction;
 };
 
+// Noise functions from https://www.shadertoy.com/view/lss3zr
+
+/////////////////////////////////////
+const mat3 m = mat3(0.00, 0.80, 0.60,
+  -0.80, 0.36, -0.48,
+  -0.60, -0.48, 0.64);
+float hash( float n )
+{
+    return fract(sin(n)*43758.5453);
+}
+
+float noise(in vec3 x)
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+
+    f = f*f*(3.0-2.0*f);
+
+    float n = p.x + p.y*57.0 + 113.0*p.z;
+
+    float res = mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                        mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y),
+                    mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                        mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
+    return res;
+}
+
+float fbm( vec3 p )
+{
+    p.xz *= rotate_2d(time / 4.0f);
+    float f;
+    f = 0.5000 * noise(p); p = m * p * 2.02;
+    f += 0.2500 * noise(p); p = m * p * 2.03;
+    f += 0.12500 * noise(p); p = m * p * 2.01;
+    f += 0.06250 * noise(p);
+    return f;
+}
+/////////////////////////////////////
+/*
+
+float noise(vec3 x ) {
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f*f*(3.0-2.0*f);
+
+  vec2 uv = (p.xy+vec2(37.0,239.0)*p.z) + f.xy;
+  vec2 tex = textureLod(noise_image,(uv+0.5)/256.0,0.0).yx;
+
+    return mix( tex.x, tex.y, f.z ) * 2.0 - 1.0;
+}
+
+float fbm(vec3 p) {
+  vec3 q = p + time * 0.5 * vec3(1.0, -0.2, -1.0);
+  float g = noise(q);
+
+  float f = 0.0;
+  float scale = 0.5;
+  float factor = 2.02;
+
+  for (int i = 0; i < 6; i++) {
+      f += scale * noise(q);
+      q *= factor;
+      factor += 0.21;
+      scale *= 0.5;
+  }
+
+  return f;
+}
+
+
+*/
 /// Ray/Line and Cube intersection test
 /// Returns a vec2 where 
 ///         x is the distance where the ray ENTERS the cube
@@ -52,11 +135,19 @@ float sd_box(vec3 p, vec3 size)
     return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
 }
 
+float sd_torus(vec3 p, vec2 t)
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return min(length(q/2)-t.y/2, length(q)-t.y);
+}
+
 float map(in vec3 p) 
 {
-    float box = sd_box(p - vec3(5.0, 2, 5.0), vec3(1.0));
+   // float d = sd_box(p - vec3(5.0, 5.0, 5.0), vec3(3.0));
+    float d = sd_spehere(p - vec3(4.5, 4.5, 4.5), 4.0);
+    // d = sd_torus(p- vec3(4.5, 4.5, 4.5), vec2(3.0, 1.0));
     
-    return -box;
+    return -d + fbm(p) / 2.0;
 }
 
 
@@ -68,11 +159,14 @@ vec4 march(vec3 ray_direction)
     vec4 final_colour = vec4(0.0);
     
     // As an optimisation, only do the raymarch if the ray intersects the volume
-    vec2 cube = interesect_cube(ray, vec3(4.0, 1.0, 4.0), vec3(2.0));
-    if (cube.x < cube.y)
+    //vec2 cube = interesect_cube(ray, vec3(0.0, 0.0,0.0), vec3(10.0));
+    //if (cube.x < cube.y)
     {
+
+        //final_colour = vec4(0.1, 0.0, 0.0, 0.0);
         // Begin marching the volume where the intersection takes place
-        float t = cube.x;
+        //float t = cube.x;
+        float t = 0.1;
         for (int i = 0; i < MAX_STEPS; i++) 
         {
             vec3 p = ray.origin + ray.direction * t;
@@ -90,7 +184,7 @@ vec4 march(vec3 ray_direction)
                 colour.rgb *= colour.a;
                 final_colour += colour * (1.0 - final_colour.a);
             }
-            else if (t > MAX_RAY_LENGTH + cube.x || t > cube.y) 
+            else if (t > MAX_RAY_LENGTH )//+ cube.x || t > cube.y) 
             {
                 break;
             }
